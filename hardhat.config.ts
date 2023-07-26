c01051308bdf9a6133836090752b93464f20b553
sowell-owen <karakulin.toshik@gmail.com>
2023-07-26 19:41:20 +0300

    chore: updated eslint and tsconfig + new packages

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,15 +2,19 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-ethers";
 
-import "./tasks/tasks";
-
 import { getEnvVars } from "./config/getEnvVars";
-import { getNetworkConfig } from "./config/getNetworkConfig";
-
+import {
+  getHardhatNetworkConfig,
+  getNetworkConfig,
+} from "./config/getNetworkConfig";
+import "hardhat-deploy";
 import "hardhat-gas-reporter";
 import "hardhat-contract-sizer";
+import "hardhat-docgen";
+import "hardhat-watcher";
+import { Networks } from "./config/types";
 
-const { OPTIMIZER, REPORT_GAS, ETHERSCAN_KEY } = getEnvVars();
+const { OPTIMIZER, REPORT_GAS, ETHERSCAN_KEY, FORKING_NETWORK } = getEnvVars();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -26,10 +30,23 @@ const config: HardhatUserConfig = {
       },
     ],
   },
+  namedAccounts: {
+    deployer: 0,
+  },
+  paths: {
+    deploy: "deploy/",
+    deployments: "deployments/",
+  },
   networks: {
-    main: getNetworkConfig("main"),
-    goerli: getNetworkConfig("goerli"),
-    testnet: getNetworkConfig("testnet"),
+    main: getNetworkConfig(Networks.MAIN),
+    goerli: getNetworkConfig(Networks.GOERLI),
+    sepolia: getNetworkConfig(Networks.SEPOLIA),
+    polygon: getNetworkConfig(Networks.POLYGON),
+    mumbai: getNetworkConfig(Networks.MUMBAI),
+    bsc: getNetworkConfig(Networks.BSC),
+    bsc_testnet: getNetworkConfig(Networks.BSC_TESTNET),
+    arbitrum: getNetworkConfig(Networks.ARBITRUM),
+    hardhat: getHardhatNetworkConfig(),
   },
   contractSizer: {
     runOnCompile: OPTIMIZER,
@@ -40,6 +57,29 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: ETHERSCAN_KEY,
   },
+  docgen: {
+    path: "./docs",
+    clear: true,
+    runOnCompile: true,
+  },
+  watcher: {
+    compilation: {
+      tasks: ["compile"],
+      files: ["./contracts"],
+      verbose: true,
+      clearOnStart: true,
+      start: "echo Running my compilation task now..",
+      runOnLaunch: true,
+    },
+  },
+  external: FORKING_NETWORK
+    ? {
+        deployments: {
+          hardhat: ["deployments/" + FORKING_NETWORK],
+          local: ["deployments/" + FORKING_NETWORK],
+        },
+      }
+    : undefined,
 };
 
 export default config;
